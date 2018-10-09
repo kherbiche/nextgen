627933b43d457f7789dfab40c04f80ce69762fc4
lyes kherbiche <kerbiche@gmail.com>
2018-10-09 03:21:46 +0200

    Gui Session & Users

---
+++ lightweightWindowing/src/main/java/dz/ummto/ansejnextgen/users/NextGenUser.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2008, 2018 Lyes Kherbiche
+ * <kerbiche@gmail.com>
+ */
+package dz.ummto.ansejnextgen.users;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * The <code>NextGenUser</code> class represents the application's user.
+ * 
+ * @author L KHERBICHE
+ * @since 0.0.1-RELEASE
+ */
+public class NextGenUser {
+
+	private String userName;
+	private String passWord;
+	private boolean enabled;
+	private Set<String> userRole = new HashSet<String>(0);
+
+	@SuppressWarnings("unused")
+	private NextGenUser() {}
+
+	public NextGenUser(String userName, String passWord, boolean enabled) {
+		super();
+		this.userName = userName;
+		this.passWord = passWord;
+		this.enabled = enabled;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassWord() {
+		return passWord;
+	}
+
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public Set<String> getUserRole() {
+		return userRole;
+	}
+
+	public void setUserRole(Set<String> userRole) {
+		this.userRole = userRole;
+	}
+
+}
---
+++ lightweightWindowing/src/main/java/dz/ummto/ansejnextgen/users/UserSession.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2008, 2018 Lyes Kherbiche
+ * <kerbiche@gmail.com>
+ */
+package dz.ummto.ansejnextgen.users;
+
+/**
+ * The <code>UserSession</code> class represents the user's session.
+ * 
+ * <p>
+ * <code>UserSession</code> encapsulate information about users
+ * 
+ * @author L KHERBICHE
+ * @since 0.0.1-RELEASE
+ */
+public class UserSession {
+
+}
