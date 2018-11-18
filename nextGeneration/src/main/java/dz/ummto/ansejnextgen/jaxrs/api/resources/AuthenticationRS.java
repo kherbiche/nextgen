e2b836ad67dd2728844eab6049b4c8a0abb1e164
lyes kherbiche <kerbiche@gmail.com>
2018-11-18 16:54:21 +0100

    Auth Resource Modif

--- nextGeneration/src/main/java/dz/ummto/ansejnextgen/jaxrs/api/resources/AuthenticationRS.java
+++ nextGeneration/src/main/java/dz/ummto/ansejnextgen/jaxrs/api/resources/AuthenticationRS.java
@@ -5,7 +5,6 @@
 package dz.ummto.ansejnextgen.jaxrs.api.resources;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -14,6 +13,8 @@ import javax.ws.rs.core.Response;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import dz.ummto.ansejnextgen.jaxrs.api.model.UserCredentials;
 
 /**
  * The <code>AuthenticationRS</code> class represents the JaxRs Resource that does
@@ -32,28 +33,15 @@ public class AuthenticationRS {
 
 	@Path("/auth")
 	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	public Response processAuthenticateUser(@FormParam("username") String username, @FormParam("password") String password) {
-
-		try {
-			/*
-			 * Authenticate the user using the credentials provided
-			 */
-			authenticate(username, password);
-			/*
-			 * Issue a token for the user
-			 */
-			String token = issueToken(username);
-			/*
-			 * Return the token on the response
-			 */
-			return Response.ok(token).build();
-		}catch(Exception e) {
-			return Response.status(Response.Status.FORBIDDEN).build();
-		}
+	/* @Consumes(MediaType.APPLICATION_FORM_URLENCODED) */
+	public Response processAuthenticateUser(UserCredentials credentials) {
+		return Response.ok("").build();
 	}
 
+	@SuppressWarnings("unused")
+	@Deprecated
 	private void authenticate(String username, String password) throws Exception {
 		/*
 		 * Authenticate against a database, LDAP, file or whatever
@@ -62,6 +50,8 @@ public class AuthenticationRS {
 		logger.info("-- authenticate()");
 	}
 
+	@SuppressWarnings("unused")
+	@Deprecated
 	private String issueToken(String username) {
 		/*
 		 * Issue a token (can be a random String persisted to a database or a JWT token)
