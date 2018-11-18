ea676d6055da4d346c4075abb89022c3a5fd5600
lyes kherbiche <kerbiche@gmail.com>
2018-11-18 04:39:55 +0100

    add security filter

---
+++ nextGeneration/src/main/java/dz/ummto/ansejnextgen/jaxrs/api/filter/AuthenticationFilter.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2008, 2018 Lyes Kherbiche
+ * <kerbiche@gmail.com>
+ */
+package dz.ummto.ansejnextgen.jaxrs.api.filter;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+
+/**
+ * The <code>AuthenticationFilter</code>
+ * 
+ * @author L KHERBICHE
+ * @since 0.0.1-RELEASE
+ */
+public class AuthenticationFilter implements ContainerRequestFilter {
+
+	@Override
+	public void filter(ContainerRequestContext requestContext) throws IOException {
+	}
+
+}
---
+++ nextGeneration/src/main/java/dz/ummto/ansejnextgen/jaxrs/api/filter/AuthorizationFilter.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2008, 2018 Lyes Kherbiche
+ * <kerbiche@gmail.com>
+ */
+package dz.ummto.ansejnextgen.jaxrs.api.filter;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+
+/**
+ * The <code>AuthorizationFilter</code>
+ * 
+ * @author L KHERBICHE
+ * @since 0.0.1-RELEASE
+ */
+public class AuthorizationFilter implements ContainerRequestFilter {
+
+	@Override
+	public void filter(ContainerRequestContext requestContext) throws IOException {
+	}
+
+}
