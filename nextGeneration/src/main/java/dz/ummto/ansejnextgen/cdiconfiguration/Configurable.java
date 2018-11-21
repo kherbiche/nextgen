123a79becfc74862eb2b0eb43d01c6626b965f13
lyes kherbiche <kerbiche@gmail.com>
2018-11-21 02:44:44 +0100

    annotation for CDI

---
+++ nextGeneration/src/main/java/dz/ummto/ansejnextgen/cdiconfiguration/Configurable.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2008, 2018 Lyes Kherbiche
+ * <kerbiche@gmail.com>
+ */
+package dz.ummto.ansejnextgen.cdiconfiguration;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.enterprise.util.Nonbinding;
+import javax.inject.Qualifier;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * The <code>Configurable</code> represents CDI qualifier for injecting
+ * configurable values.
+ * 
+ * @author L KHERBICHE
+ * @since 0.0.1-RELEASE
+ */
+@Qualifier
+@Retention(RUNTIME)
+@Target({ TYPE, METHOD, FIELD, PARAMETER })
+public @interface Configurable {
+
+	@Nonbinding
+	String value() default "";
+}
