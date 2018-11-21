ee5bada4d970125d70f2d13e39d98d86a6147cfa
lyes kherbiche <kerbiche@gmail.com>
2018-11-21 19:46:25 +0100

    Auth Token Details

---
+++ nextGeneration/src/main/java/dz/ummto/ansejnextgen/jaxrs/Authority.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2008, 2018 Lyes Kherbiche
+ * <kerbiche@gmail.com>
+ */
+package dz.ummto.ansejnextgen.jaxrs;
+
+/**
+ * The <code>Authority</code> Represents an authority granted to a
+ * {@link dz.ummto.ansejNextGen.jpa.entities.User}.
+ * 
+ * @author L KHERBICHE
+ * @since 0.0.1-RELEASE
+ */
+public enum Authority {
+
+	ADMIN, CONSELOR
+}
