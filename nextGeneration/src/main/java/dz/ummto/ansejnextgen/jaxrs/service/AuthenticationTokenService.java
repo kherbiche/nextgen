eabb48910664641884809d01b0850a2ad603267f
lyes kherbiche <kerbiche@gmail.com>
2018-11-21 03:29:41 +0100

    properties for security

---
+++ nextGeneration/src/main/java/dz/ummto/ansejnextgen/jaxrs/service/AuthenticationTokenService.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2008, 2018 Lyes Kherbiche
+ * <kerbiche@gmail.com>
+ */
+package dz.ummto.ansejnextgen.jaxrs.service;
+
+import javax.inject.Inject;
+
+import dz.ummto.ansejnextgen.cdiconfiguration.Configurable;
+
+/**
+ * The <code>AuthenticationTokenService</code>
+ * 
+ * @author L KHERBICHE
+ * @since 0.0.1-RELEASE
+ */
+public class AuthenticationTokenService {
+
+	@Inject
+	@Configurable("authentication.jwt.validFor")
+	private Long validFor;
+
+	@Inject
+	@Configurable("authentication.jwt.refreshLimit")
+	private Integer refreshLimit;
+
+}
