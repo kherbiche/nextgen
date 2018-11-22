fb3ad263a0609052d59f93b34411648685f3d8a8
lyes kherbiche <kerbiche@gmail.com>
2018-11-22 20:32:08 +0100

    SecurityContext impl

---
+++ nextGeneration/src/main/java/dz/ummto/ansejnextgen/jaxrs/api/TokenBasedSecurityContext.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2008, 2018 Lyes Kherbiche
+ * <kerbiche@gmail.com>
+ */
+package dz.ummto.ansejnextgen.jaxrs.api;
+
+import java.security.Principal;
+
+import javax.ws.rs.core.SecurityContext;
+
+import dz.ummto.ansejnextgen.jaxrs.Authority;
+
+/**
+ * The <code>TokenBasedSecurityContext</code> represents {@link SecurityContext}
+ * implementation for token-based authentication
+ * 
+ * @author L KHERBICHE
+ * @since 0.0.1-RELEASE
+ */
+
+public class TokenBasedSecurityContext implements SecurityContext {
+
+	private AuthenticatedUserDetails authenticatedUserDetails;
+	private AuthenticationTokenDetails authenticationTokenDetails;
+	private final boolean secure;
+
+	public TokenBasedSecurityContext(AuthenticatedUserDetails authenticatedUserDetails,
+			AuthenticationTokenDetails authenticationTokenDetails, boolean secure) {
+		this.authenticatedUserDetails = authenticatedUserDetails;
+		this.authenticationTokenDetails = authenticationTokenDetails;
+		this.secure = secure;
+	}
+
+	@Override
+	public String getAuthenticationScheme() {
+		return "Yugerten";
+	}
+
+	@Override
+	public Principal getUserPrincipal() {
+		return authenticatedUserDetails;
+	}
+
+	@Override
+	public boolean isSecure() {
+		return secure;
+	}
+
+	@Override
+	public boolean isUserInRole(String role) {
+		return authenticationTokenDetails.getAuthorities().contains(Authority.valueOf(role));
+	}
+
+}
