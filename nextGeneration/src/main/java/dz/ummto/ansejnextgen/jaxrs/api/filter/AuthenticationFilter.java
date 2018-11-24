6f11f06862933fe35572b5daefc4bfdb1833f5d6
lyes kherbiche <kerbiche@gmail.com>
2018-11-24 20:00:47 +0100

    Jax-Rs Authentication Filter

--- nextGeneration/src/main/java/dz/ummto/ansejnextgen/jaxrs/api/filter/AuthenticationFilter.java
+++ nextGeneration/src/main/java/dz/ummto/ansejnextgen/jaxrs/api/filter/AuthenticationFilter.java
@@ -5,6 +5,8 @@
 package dz.ummto.ansejnextgen.jaxrs.api.filter;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.annotation.Priority;
 import javax.enterprise.context.Dependent;
@@ -12,16 +14,22 @@ import javax.inject.Inject;
 import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.ext.Provider;
 
 import com.google.common.net.HttpHeaders;
 
+import dz.ummto.ansejNextGen.jpa.entities.User;
+import dz.ummto.ansejNextGen.jpa.entities.UserRole;
+import dz.ummto.ansejnextgen.jaxrs.Authority;
+import dz.ummto.ansejnextgen.jaxrs.api.AuthenticatedUserDetails;
 import dz.ummto.ansejnextgen.jaxrs.api.AuthenticationTokenDetails;
+import dz.ummto.ansejnextgen.jaxrs.api.TokenBasedSecurityContext;
 import dz.ummto.ansejnextgen.jaxrs.service.AuthenticationTokenService;
 import dz.ummto.ansejnextgen.jaxrs.service.UserService;
 
 /**
- * The <code>AuthenticationFilter</code>
+ * The <code>AuthenticationFilter</code> represents JWT authentication filter.
  * 
  * @author L KHERBICHE
  * @since 0.0.1-RELEASE
@@ -55,7 +63,18 @@ public class AuthenticationFilter implements ContainerRequestFilter {
 	}
 
 	private void handleTokenBasedAuthentication(String authenticationToken, ContainerRequestContext requestContext) {
-		AuthenticationTokenDetails  authToDet = authenticationTokenService.parseToken(authenticationToken);
+
+		AuthenticationTokenDetails authToDet = authenticationTokenService.parseToken(authenticationToken);
+		User user = userService.findById(authToDet.getUsername());
+		Set<Authority> setAuth = new HashSet<Authority>();
+		for (UserRole ur : user.getUserRoles()) {
+			setAuth.add(Authority.valueOf(ur.getRole().name()));
+		}
+
+		AuthenticatedUserDetails authUserDet = new AuthenticatedUserDetails(user.getUserName(), setAuth);
+		boolean secure = requestContext.getSecurityContext().isSecure();
+		SecurityContext secContext = new TokenBasedSecurityContext(authUserDet, authToDet, secure);
+		requestContext.setSecurityContext(secContext);
 	}
 
 }
