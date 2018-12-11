895f44cafec497256990547688bb879fc79bede5
lyes kherbiche <kerbiche@gmail.com>
2018-12-11 20:32:36 +0100

    ud

--- build.sh
+++ build.sh
@@ -7,18 +7,18 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "Syphax Building and analyzing a regular branch"
   
   if [ "$TRAVIS_BRANCH" == "master" ]; then
-    mvn -DskipTests clean install sonar:sonar \
+    mvn clean install sonar:sonar \
 	  -Dsonar.organization=$SONAR_ORGANIZATION \
 	  -Dsonar.host.url=$SONAR_HOST_URL \
 	  -Dsonar.login=$SONAR_TOKEN
   elif [ "$TRAVIS_BRANCH" == "release" ]; then
-    mvn -DskipTests clean install sonar:sonar \
+    mvn clean install sonar:sonar \
           -Prelease
 	  -Dsonar.organization=$SONAR_ORGANIZATION \
 	  -Dsonar.host.url=$SONAR_HOST_URL \
 	  -Dsonar.login=$SONAR_TOKEN
   else
-    mvn -DskipTests clean verify sonar:sonar \
+    mvn clean verify sonar:sonar \
 	  -Dsonar.organization=$SONAR_ORGANIZATION \
 	  -Dsonar.host.url=$SONAR_HOST_URL \
 	  -Dsonar.login=$SONAR_TOKEN \
