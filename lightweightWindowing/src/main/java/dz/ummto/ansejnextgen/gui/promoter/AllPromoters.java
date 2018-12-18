8892f64b53073aab534f2bdd16eefb679110117c
lyes kherbiche <kerbiche@gmail.com>
2018-12-18 23:59:56 +0100

    all promoters gui

---
+++ lightweightWindowing/src/main/java/dz/ummto/ansejnextgen/gui/promoter/AllPromoters.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2008, 2018 Lyes Kherbiche
+ * <kerbiche@gmail.com>
+ */
+package dz.ummto.ansejnextgen.gui.promoter;
+
+import javax.swing.JPanel;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * The <code>AllPromoters</code> class represents .
+ * 
+ * @author L KHERBICHE
+ * @since 0.0.1-RELEASE
+ */
+
+@SuppressWarnings("serial")
+public class AllPromoters extends JPanel {
+
+	private static final Log logger = LogFactory.getLog(AllPromoters.class);
+}
