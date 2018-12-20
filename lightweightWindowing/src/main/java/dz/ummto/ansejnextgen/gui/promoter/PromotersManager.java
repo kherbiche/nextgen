1114a0c29da46e26f652a9da1157373734fa6154
lyes kherbiche <kerbiche@gmail.com>
2018-12-20 04:48:58 +0100

    Promoters Manager Panel

--- lightweightWindowing/src/main/java/dz/ummto/ansejnextgen/gui/promoter/PromotersManager.java
+++ lightweightWindowing/src/main/java/dz/ummto/ansejnextgen/gui/promoter/PromotersManager.java
@@ -4,7 +4,18 @@
  */
 package dz.ummto.ansejnextgen.gui.promoter;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingUtilities;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -17,7 +28,81 @@ import org.apache.commons.logging.LogFactory;
  */
 
 @SuppressWarnings("serial")
-public class AllPromoters extends JPanel {
+public class PromotersManager extends JPanel {
 
-	private static final Log logger = LogFactory.getLog(AllPromoters.class);
+	private static final Log logger = LogFactory.getLog(PromotersManager.class);
+
+	private PromoTableModel tableModel;
+	private JTable table;
+
+	public PromotersManager() {
+		Runnable code = new Runnable() {
+			public void run() {
+				jbInit();
+			}
+		};
+		if (SwingUtilities.isEventDispatchThread()) {
+			code.run();
+		} else {
+			SwingUtilities.invokeLater(code);
+		}
+	}
+
+	private void jbInit() {
+
+		this.setLayout(null);
+
+		JPanel tablePanel = new JPanel();
+		tablePanel.setBorder(BorderFactory.createTitledBorder("Promoters manager"));
+		tablePanel.setFont(new java.awt.Font(Font.DIALOG, 1, 14));
+		tablePanel.setBackground(new Color(0, 0, 0, 230));
+		tablePanel.setBounds(new Rectangle(2, 0, 700, 120));
+		tablePanel.setLayout(null);
+
+		JPanel boutonsPanel = new JPanel();
+		boutonsPanel.setFont(new java.awt.Font(Font.DIALOG, 1, 14));
+		boutonsPanel.setBackground(new Color(0, 0, 0, 230));
+		boutonsPanel.setBounds(new Rectangle(2, 125, 700, 113));
+		boutonsPanel.setLayout(null);
+		boutonsPanel.add(new JButton(new AddRowsAction()));
+		boutonsPanel.add(new JButton(new RemoveRowsAction()));
+
+		/* TODO this must be run on SwingWorker */
+		tableModel = new PromoTableModel();
+		/* end */
+		table = new JTable(tableModel);
+		table.setAutoCreateRowSorter(true);
+
+		tablePanel.add(new JScrollPane(table), null);
+
+		this.add(tablePanel, null);
+	}
+
+	private class AddRowsAction extends AbstractAction {
+
+		private AddRowsAction() {
+			super("Add row");
+		}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			logger.info("-- AddRowsAction.actionPerformed()");
+			//TODO
+		}
+	}
+
+	private class RemoveRowsAction extends AbstractAction {
+
+		private RemoveRowsAction() {
+			super("Delete row");
+		}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			logger.info("-- RemoveRowsAction.actionPerformed()");
+			int[] selection = table.getSelectedRows();
+			 
+			for(int i = selection.length - 1; i >= 0; i--){
+				tableModel.removeData(selection[i]);
+			}
+		}
+	}
 }
