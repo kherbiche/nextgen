420e41adb9d99f0fd8fdabf7d6a33e6663df36ea
lyes kherbiche <kerbiche@gmail.com>
2019-02-05 22:51:34 +0100

    PromoTableModel getColumnClass impl

--- lightweightWindowing/src/main/java/dz/ummto/ansejnextgen/gui/promoter/PromoTableModel.java
+++ lightweightWindowing/src/main/java/dz/ummto/ansejnextgen/gui/promoter/PromoTableModel.java
@@ -67,6 +67,40 @@ public class PromoTableModel extends AbstractTableModel {
 	}
 
 	@Override
+	public Class<?> getColumnClass(int columnIndex) {
+		switch (columnIndex) {
+		case 0:
+			return String.class;
+		case 1:
+			return String.class;
+		case 2:
+			return String.class;
+		case 3:
+			return String.class;
+		case 4:
+			return String.class;
+		case 5:
+			return String.class;
+		case 6:
+			return Integer.class;
+		case 7:
+			return Integer.class;
+		case 8:
+			return String.class;
+		case 9:
+			return String.class;
+		case 10:
+			return String.class;
+		case 11:
+			return Integer.class;
+		case 12:
+			return String.class;
+		default:
+			return Object.class;
+		}
+	}
+
+	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
 		switch (columnIndex) {
 		case 0:
