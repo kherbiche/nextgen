af70e9cc6b00eac573de9e111ddb0aa8180c47a2
kherbiche <kerbiche@gmail.com>
2022-09-17 00:34:41 +0200

    login module

--- ansejFront/src/app/login/login.component.ts
+++ ansejFront/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+  }
+  
+  loginAnsej (): void {
+	  let login:any  = (<HTMLInputElement>window.document.getElementById('typeEmailX')).value;
+	  let password:any  = (<HTMLInputElement>window.document.getElementById('typePasswordX')).value;
+	  const url = 'http://192.168.1.109:8089/rest/auth';
+      //let formData: FormData = new FormData();
+      //formData.append('login', login);
+      //formData.append('password', password);
+      const postData  = [login, password];
+
+      // //let headers = new HttpHeaders().set('access-control-allow-origin',"http://192.168.1.109:8089");
+      //let headers = new HttpHeaders();
+      //headers.append('access-control-allow-origin',"*");
+      //headers.append('Authorization', "Yugarten ");
+      //let options = {headers: headers}
+	    
+	  /*const headers = new HttpHeaders({
+        'content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Authorization': 'Yugarten '
+      });*/
+      
+      
+      const headers = new HttpHeaders().set('Authorization','Yugarten ');
+      
+	  const options = { headers: headers };
+      
+      
+      this.http.post(url, postData, options).subscribe();
+
   }
 
 }
