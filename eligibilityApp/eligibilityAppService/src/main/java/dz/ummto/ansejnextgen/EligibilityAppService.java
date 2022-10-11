351f9e93b45c10e6761eb986140809367e711e2e
kherbiche <kerbiche@gmail.com>
2022-10-11 14:57:43 +0200

    home controller

---
+++ eligibilityApp/eligibilityAppService/src/main/java/dz/ummto/ansejnextgen/EligibilityAppService.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2008, 2022 Lyes Kherbiche
+ * <kerbiche@gmail.com>
+ */
+package dz.ummto.ansejnextgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author L KHERBICHE
+ * @since 0.0.1-RELEASE
+ */
+@SpringBootApplication
+public class EligibilityAppService {
+	
+	public static void main(String[] args){
+		SpringApplication.run(EligibilityAppService.class, args);
+	}
+}
---
+++ eligibilityApp/eligibilityAppService/src/main/java/dz/ummto/controller/Home.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2008, 2022 Lyes Kherbiche
+ * <kerbiche@gmail.com>
+ */
+package dz.ummto.ansejnextgen.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 
+ * @author L KHERBICHE
+ * @since 0.0.1-RELEASE
+ */
+@RestController
+@RequestMapping(value = "/rest")
+public class Home{
+	
+	@RequestMapping(value = "/test", method = RequestMethod.GET, produces="application/json")
+	public String[] test() {
+		String[] str = {"ca","marche","bien"};
+		return str;
+	}
+}
