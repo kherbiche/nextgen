e7757bd336dd11acfa00b8dce5c848789ac86a61
kherbiche <kerbiche@gmail.com>
2022-10-13 16:02:12 +0200

    link servive eligibility with repo

--- eligibilityApp/eligibilityAppService/src/main/java/dz/ummto/ansejnextgen/controller/Home.java
+++ eligibilityApp/eligibilityAppService/src/main/java/dz/ummto/ansejnextgen/controller/Home.java
@@ -7,9 +7,19 @@ package dz.ummto.ansejnextgen.controller;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import dz.ummto.ansejNextGen.jpa.dao.DaoFactory;
+import dz.ummto.ansejNextGen.jpa.dao.IDao;
+import dz.ummto.ansejNextGen.jpa.entities.Eligibility;
+import dz.ummto.ansejNextGen.jpa.entities.Promoter;
+import dz.ummto.ansejNextGen.jpa.entities.PromoterId;
 
 /**
  * 
@@ -18,9 +28,43 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @RestController
 @RequestMapping(value = "/rest")
-public class Home{
+public class Home {
 	
 	private static final Log logger = LogFactory.getLog(Home.class);
+
+	/**
+	 * curl -X POST localhost:8088/rest/eligibility -H "Content-Type:application/json" -d '{"firstName":"foo","lastName":"bar","birthDate":"01/"}'
+	 * 
+	 */
+	@PostMapping("/eligibility")
+	public void makeEligible(@RequestBody Map<String, String> eligibilityMap) {
+		logger.info("--- URI: /rest/eligibility");
+		logger.info("--- POST Content 0: " + eligibilityMap.get("lastName"));
+		logger.info("--- POST Content 1: " + eligibilityMap.get("firstName"));
+		logger.info("--- POST Content 1: " + eligibilityMap.get("birthDate"));
+
+		IDao<Long, Eligibility> eDao = DaoFactory.getEligibilityDao();
+		try {
+			Eligibility e = new Eligibility();
+			Promoter p = new Promoter();
+			PromoterId pid = new PromoterId();
+			
+			pid.setLastName("kh");
+			pid.setFirstName("lys");
+			pid.setBirthDate("12/01/1990");
+			
+			p.setPromoterId(pid);
+			
+			e.setPromoter(p);
+			e.setDecisionDate(eligibilityMap.get("decisionDate"));
+			e.setStatus(eligibilityMap.get("status").equals("true") ? true : false);
+			e.setComment(eligibilityMap.get("comment"));
+			
+			eDao.save(e);
+		} finally {
+			eDao.closeResource();
+		}
+	}
 	
 	@RequestMapping(value = "/test", method = RequestMethod.GET, produces="application/json")
 	public String[] test() {
